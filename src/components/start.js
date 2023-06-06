5328d0a58b7ba296029eac5e8a6638ab7e6fbd3a
Suprovho <106928567+Suprovho@users.noreply.github.com>
2023-06-06 22:20:25 +0530

    upadted

--- src/components/start.js
+++ src/components/start.js
@@ -2,7 +2,7 @@ export default function Start(props) {
     return(
         <div className="start-container">
             <h1 className="start-heading">Quizzical</h1>
-            <p className="start-p">Some description if needed</p>
+            <p className="start-p">Quick and easy quiz game</p>
             <button className="start-button" onClick={props.startQuiz}>Start quiz</button>
         </div>
     )
